605f07de1b917e61de4e2e74778d6ff19369b8f8
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-02 14:08:48 -0800

    Added react component definition to launch page

--- client/src/pages/launch.js
+++ client/src/pages/launch.js
@@ -25,3 +25,24 @@ export const GET_LAUNCH_DETAILS = gql`
     }
   }
 `;
+
+export default function Launch({ launchId }) {
+  return (
+    <Query query={GET_LAUNCH_DETAILS} variables={{ launchId }}>
+      {({ data, loading, error }) => {
+        if (loading) return <Loading />;
+        if (error) return <p>ERROR: {error.message}</p>;
+
+        return (
+          <Fragment>
+            <Header image={data.launch.mission.missionPatch}>
+              {data.launch.mission.name}
+            </Header>
+            <LaunchDetail {...data.launch} />
+            <ActionButton {...data.launch} />
+          </Fragment>
+        );
+      }}
+    </Query>
+  );
+}
