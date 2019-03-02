72cce5271943a40654474f4a0fa1827dddc3e121
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-02 12:05:52 -0800

    Created initial apollo provider and react dom integration

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,11 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import gql from "graphql-tag";
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { ReactDOM } from 'react-apollo';
+
+import Pages from '.pages';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -13,16 +18,9 @@ const client = new ApolloClient({
   link
 });
 
-client.query({
-  query: gql`
-    query GetLaunch {
-      launch(id: 56) {
-        id
-        mission {
-          name
-        }
-      }
-    }
-  `
-})
-.then(result => console.log(result));
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>, document.getElementById('root'));
